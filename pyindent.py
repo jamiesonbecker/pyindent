b67dea9fee89a12feb87124a7a249ce2594d17e6
Jamieson Becker <jamieson@jamiesonbecker.com>
2015-02-16 11:44:51 -0600

    Update pyindent.py

--- pyindent.py
+++ pyindent.py
@@ -1,3 +1,4 @@
 def pyindent(text, indent=4, truncate=0):
-    return ("\n").join([(indent * " " + line[:truncate] if truncate else line)
-        for line in text.split("\n")])
+    return ("\n").join(
+        [(indent * " " + line[:truncate] if truncate else line)
+            for line in text.split("\n")])
