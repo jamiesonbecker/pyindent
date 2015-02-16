f9d6002c647e9e3e9d646f26dbcb161f0d60c8d5
Jamieson Becker <jamieson@jamiesonbecker.com>
2015-02-16 11:54:14 -0600

    Added ellipsis to truncated line

--- pyindent.py
+++ pyindent.py
@@ -1,4 +1,4 @@
 def pyindent(text, indent=4, truncate=0):
     return ("\n").join(
-        [(indent * " " + line[:truncate] if truncate else line)
+        [(indent * " " + line[:truncate] + " ..." if truncate else line)
             for line in text.split("\n")])
