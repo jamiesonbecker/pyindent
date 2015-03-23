7c87f1bd809e13f44891d744792e79f7ac7e6a40
Jamieson Becker <jamieson@jamiesonbecker.com>
2015-03-23 19:20:18 +0000

    updated for pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.2.0',
 
     description='A multi-line indenter',
     long_description='Pyindent is a super-simple multi-line indenting library for console output and debugging.',
@@ -20,7 +20,7 @@ setup(
     # The project's main homepage.
     url='https://github.com/jamiesonbecker/pyindent',
 
-    download_url="https://github.com/jamiesonbecker/pyindent/tarball/0.1",
+    download_url="https://github.com/jamiesonbecker/pyindent/archive/master.tar.gz",
 
     # Author details
     author='Jamieson Becker',
